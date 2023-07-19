f413c80a104b6fb0ea65156480600e5b53b2f032
Toi Tran <toitv.astary@gmail.com>
2023-07-19 17:21:30 +0700

    update project

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -118,9 +118,11 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
+MEDIA_URL = 'image/'
 STATICFILES_DIRS = [
     BASE_DIR / 'staticfiles/static'
 ]
+MEDIA_ROOT = BASE_DIR / 'staticfiles/static/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- store/views.py
+++ store/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def store(request):
-    context = {}
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
     return render(request, 'store.html', context)
 def cart(request):
     context = {}
